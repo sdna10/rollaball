652660a426f009d45d6d363a1e1162989f82053a
Dana Spillinger <sdna10@cs.washington.edu>
2019-01-16 20:47:28 -0800

    Done!

--- RollaBall/Assets/Scripts/CameraController.cs
+++ RollaBall/Assets/Scripts/CameraController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public GameObject player;
+    private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = transform.position - player.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = player.transform.position + offset;
     }
 }
