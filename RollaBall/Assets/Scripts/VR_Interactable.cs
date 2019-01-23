d460d7a7e6e1f93c7ac9a2c3c754d8c9ea847dfc
Dana Spillinger <sdna10@cs.washington.edu>
2019-01-22 16:06:52 -0800

    Progress

---
+++ RollaBall/Assets/Scripts/VR_Interactable.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class VR_Interactable : MonoBehaviour
+
+{
+
+    protected Material previousMat = null;
+
+    protected Vector3 interactorHitPosition = Vector3.zero;
+
+    protected bool selected = false;
+    private bool spotted;
+    public Animator m_Animator;
+
+
+    // Start is called before the first frame update
+
+    public virtual void OnSelectEnter()
+
+    {
+
+        var meshRenderer = this.GetComponent<MeshRenderer>();
+
+        if (meshRenderer != null)
+
+        {
+
+            previousMat = meshRenderer.material;
+
+            meshRenderer.material = selectedMat;
+
+        }
+        
+        selected = true;
+        spotted = true;
+        m_Animator = gameObject.GetComponent<Animator>();
+        m_Animator.SetBool("spotted", spotted);
+    }
+
+
+
+    public virtual void OnSelectExit()
+
+    {
+
+        var meshRenderer = this.GetComponent<MeshRenderer>();
+
+        if (meshRenderer != null && previousMat != null)
+
+        {
+
+            meshRenderer.material = previousMat;
+
+        }
+
+        selected = false;
+        spotted = false;
+        m_Animator = gameObject.GetComponent<Animator>();
+        m_Animator.SetBool("spotted", spotted);
+    }
+
+
+
+
+
+    public virtual void InteractorPosition(Vector3 position)
+
+    {
+
+        interactorHitPosition = position;
+
+    }
+
+
+
+    public virtual void OnInteract()
+
+    {
+
+        return;
+
+    }
+
+
+
+    public Material selectedMat = null;
+
+}
---
+++ RollaBall/Assets/Scripts/VR_Interactor.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class VR_Interactor : MonoBehaviour
+
+{
+
+    private VR_Interactable selected = null;
+
+
+    public LayerMask layerMask;
+    // Update is called once per frame
+
+    void Update()
+
+    {
+
+
+
+        RaycastHit hit;
+
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 100.0f, Color.yellow);
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 100.0f, layerMask))
+
+        {
+
+            if (hit.transform != null)
+
+            {
+
+                var interactable = hit.transform.GetComponent<VR_Interactable>();
+
+                if (interactable != null)
+
+                {
+
+                    if (interactable != selected)
+
+                    {
+
+                        if (selected != null)
+
+                        {
+
+                            selected.OnSelectExit();
+
+                        }
+
+                        selected = interactable;
+
+                        selected.OnSelectEnter();
+
+                    }
+
+                    selected.InteractorPosition(hit.point);
+
+                    if ((Input.GetAxis("XRI_Right_Trigger") > 0.5f) || (Input.GetAxis("XRI_Left_Trigger") > 0.5f))
+
+                    {
+
+                        selected.OnInteract();
+
+                    }
+
+                    return;
+
+                }
+
+            }
+
+        }
+
+        if (selected != null)
+
+        {
+
+            selected.OnSelectExit();
+
+            selected = null;
+
+        }
+
+    }
+
+}
